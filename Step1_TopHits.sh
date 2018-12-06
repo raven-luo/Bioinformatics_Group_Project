6b0bf2e216c44aec8ce97a11e587936ed24ff3fc
Raven Luo <yluo3@nd.edu>
2018-12-06 18:01:03 -0500

    More comments on all scripts except for R

--- Step1_TopHits.sh
+++ Step1_TopHits.sh
@@ -1,6 +1,6 @@
 # Step1_TopHits.sh
 # Usage: bash Step1_TopHits.sh
-# find the top hits for 6 transcript proteins
+# find the top hits for each of 6 transcript proteins and write them into the csv file TopHits.csv
 
 for HitTable in HitTable*.csv
 do
